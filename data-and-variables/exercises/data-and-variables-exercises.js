bb53f361216260284565c22f0c906fc34bb39f60
Dagmawe Assefa <dassefaog@Dagmawes-MacBook-Pro.local>
2024-06-15 15:15:27 -0500

    <final data and variables>

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,12 +1,40 @@
 // Declare and assign the variables below
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKm = 0.621;
 
-let classtime = "fun";
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-console.log("ello");
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeedMph);
+console.log(typeof distanceToMarsKm);
+console.log(typeof distanceToMoonKm);
+console.log(typeof milesPerKm);
+
 // Calculate a space mission below
+
+let milesToMars = distanceToMarsKm * milesPerKm;
+let hoursToMars = milesPerKm / shuttleSpeedMph;
+let daysToMars = distanceToMarsKm / 24;
 
 // Print the results of the space mission calculations below
 
+console.log(milesToMars);
+console.log(hoursToMars);
+console.log(daysToMars);
+
+"it will take 9375000 days to reach Mars"
+
 // Calculate a trip to the moon below
 
+let milesToMoon = distanceToMoonKm * milesPerKm;
+let hoursToMoon = milesPerKm / shuttleSpeedMph;
+let daysToMoon = distanceToMoonKm /24;
+
+console.log(milesToMoon);
+console.log(hoursToMoon);
+console.log(daysToMoon);
+
 // Print the results of the trip to the moon below
+"it will take 1600 days to reach the moon"
