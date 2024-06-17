ceb25e35072f4f25e9561da676618594563dd995
Dagmawe Assefa <dassefaog@Dagmawes-MacBook-Pro.local>
2024-06-17 12:34:28 -0500

    <error and debug exercise>

--- errors-and-debugging/exercises/DebuggingLogicErrors3.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors3.js
@@ -26,6 +26,7 @@ if (crewStatus && computerStatus === 'green'){
    launchReady = false;
 }
 
+
 // if (launchReady) {
 //    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
 //    console.log('Liftoff!');
